5ff8c9dd31e280bd8ac62d968b4cdbeaeadee9be
helano <helano@github.com>
2020-01-05 12:26:40 -0300

    atualizando app

---
+++ src/main/java/com/example/demo/controllers/TesteController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controllers;
+
+import javax.annotation.security.PermitAll;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@PermitAll
+@RequestMapping("teste")
+public class TesteController {
+
+	@GetMapping("/controller")
+	public String  teste() {
+
+		return "<h1> We Are Running on Heroku! :) </h1>";
+	}
+	
+	
+}
